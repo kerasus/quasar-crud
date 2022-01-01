3f03249a609c5b411dbbcad8d74c92611a145690
Ali <aliesmaeeli20@gmail.com>
2022-01-01 12:21:13 +0330

    add Portlet

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ import EntityCreate from './src/components/Entity/Create/EntityCreate'
 import EntityShow from './src/components/Entity/Show/EntityShow'
 import EntityEdit from './src/components/Entity/Edit/EntityEdit'
 import EntityIndex from './src/components/Entity/Index/EntityIndex'
+import Portlet from './src/components/Portlet'
 
-export {EntityCreate, EntityShow, EntityEdit, EntityIndex}
+export {EntityCreate, EntityShow, EntityEdit, EntityIndex, Portlet}
