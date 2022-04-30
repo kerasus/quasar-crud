06209dfaf6152e52d8498c7024bb0afa5ceb9fc1
JasGH <yasaman.gonehi@gmail.com>
2022-04-30 15:07:00 +0430

    input name bug fix

--- src/mixins/EntityMixin.js
+++ src/mixins/EntityMixin.js
@@ -32,7 +32,7 @@ const EntityMixin = {
         for (let i = 0; i < inputs.length; i++) {
           const input = inputs[i]
           if (input.type !== 'formBuilder') {
-            if (input.name.toString() === entityIdKey) {
+            if (input.name && input.name.toString() === entityIdKey) {
               return input
             }
           } else {
