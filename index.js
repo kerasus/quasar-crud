5e99c8ffa5a81cd1f690f67bbae0b89cd8b64394
kerasus <aliesmaeeli20@gmail.com>
2022-12-28 12:37:30 +0330

    module.exports.default

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict'
 
-import EntityCreate from './src/components/Entity/Create/EntityCreate'
-import EntityShow from './src/components/Entity/Show/EntityShow'
-import EntityEdit from './src/components/Entity/Edit/EntityEdit'
-import EntityIndex from './src/components/Entity/Index/EntityIndex'
-import EntityCrud from './src/components/EntityCrud'
-import EntityIndexTable from './src/components/Entity/Index/EntityIndexTable'
-import EntityInput from './src/components/Entity/Attachment/EntityInput'
-import Portlet from './src/components/Portlet'
-import EntityCrudFormBuilder from './src/components/Entity/EntityCrudFormBuilder'
-import EntityAction from './src/components/Entity/EntityAction.vue'
+const EntityCreate = require('./src/components/Entity/Create/EntityCreate')
+const EntityShow = require('./src/components/Entity/Show/EntityShow')
+const EntityEdit = require('./src/components/Entity/Edit/EntityEdit')
+const EntityIndex = require('./src/components/Entity/Index/EntityIndex')
+const EntityCrud = require('./src/components/EntityCrud')
+const EntityIndexTable = require('./src/components/Entity/Index/EntityIndexTable')
+const EntityInput = require('./src/components/Entity/Attachment/EntityInput')
+const Portlet = require('./src/components/Portlet')
+const EntityCrudFormBuilder = require('./src/components/Entity/EntityCrudFormBuilder')
+const EntityAction = require('./src/components/Entity/EntityAction.vue')
+const EntityCrudObject = { EntityCreate, EntityShow, EntityEdit, EntityIndex, EntityCrud, Portlet, EntityIndexTable, EntityInput, EntityCrudFormBuilder, EntityAction }
 
-export { EntityCreate, EntityShow, EntityEdit, EntityIndex, EntityCrud, Portlet, EntityIndexTable, EntityInput, EntityCrudFormBuilder, EntityAction }
+module.exports = EntityCrudObject;
+module.exports.default = EntityCrudObject;
