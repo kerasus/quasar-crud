40cd3dc1e5b1dab63b98e4e0bbd6ddbf9fabceae
kerasus <aliesmaeeli20@gmail.com>
2022-12-31 15:09:58 +0330

    require default

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-'use strict'
-
-const Portlet = require('./src/components/Portlet.vue')
-const EntityCrud = require('./src/components/EntityCrud.vue')
-const EntityAction = require('./src/components/Entity/EntityAction.vue')
-const EntityShow = require('./src/components/Entity/Show/EntityShow.vue')
-const EntityEdit = require('./src/components/Entity/Edit/EntityEdit.vue')
-const EntityIndex = require('./src/components/Entity/Index/EntityIndex.vue')
-const EntityCreate = require('./src/components/Entity/Create/EntityCreate.vue')
-const EntityInput = require('./src/components/Entity/Attachment/EntityInput.vue')
-const EntityIndexTable = require('./src/components/Entity/Index/EntityIndexTable.vue')
-const EntityCrudFormBuilder = require('./src/components/Entity/EntityCrudFormBuilder.vue')
+const Portlet = require('./src/components/Portlet.vue').default
+const EntityCrud = require('./src/components/EntityCrud.vue').default
+const EntityAction = require('./src/components/Entity/EntityAction.vue').default
+const EntityShow = require('./src/components/Entity/Show/EntityShow.vue').default
+const EntityEdit = require('./src/components/Entity/Edit/EntityEdit.vue').default
+const EntityIndex = require('./src/components/Entity/Index/EntityIndex.vue').default
+const EntityCreate = require('./src/components/Entity/Create/EntityCreate.vue').default
+const EntityInput = require('./src/components/Entity/Attachment/EntityInput.vue').default
+const EntityIndexTable = require('./src/components/Entity/Index/EntityIndexTable.vue').default
+const EntityCrudFormBuilder = require('./src/components/Entity/EntityCrudFormBuilder.vue').default
 const EntityCrudObject = { EntityCreate, EntityShow, EntityEdit, EntityIndex, EntityCrud, Portlet, EntityIndexTable, EntityInput, EntityCrudFormBuilder, EntityAction }
 
-module.exports = EntityCrudObject;
-module.exports.default = EntityCrudObject;
+export {
+  EntityCrudObject as default,
+  EntityCreate,
+  EntityShow,
+  EntityEdit,
+  EntityIndex,
+  EntityCrud,
+  Portlet,
+  EntityIndexTable,
+  EntityInput,
+  EntityCrudFormBuilder,
+  EntityAction
+}
