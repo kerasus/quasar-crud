a1b11b881fbf58ae3e97c9838c16d511a7e91c0c
JasGH <yasaman.gonehi@gmail.com>
2023-05-20 18:47:59 +0330

    test page updated

--- src/TestPages/List.vue
+++ src/TestPages/List.vue
@@ -15,23 +15,23 @@
     <template #no-entity>
       THERE IS NOTHING TO SHOW RIGHT NOW.
     </template>
-    <template #entity-index-table-cell="{props, col, showConfirmRemoveDialog}">
-      <template v-if="col.name === 'thumbnail'">
+    <template #entity-index-table-cell="{inputData, showConfirmRemoveDialog}">
+      <template v-if="inputData.col.name === 'thumbnail'">
         <q-avatar>
-          <q-img :src="col.value"
+          <q-img :src="inputData.col.value"
                  placeholder-src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWBAMAAADOL2zRAAAAG1BMVEXMzMyWlpaqqqq3t7fFxcW+vr6xsbGjo6OcnJyLKnDGAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABAElEQVRoge3SMW+DMBiE4YsxJqMJtHOTITPeOsLQnaodGImEUMZEkZhRUqn92f0MaTubtfeMh/QGHANEREREREREREREtIJJ0xbH299kp8l8FaGtLdTQ19HjofxZlJ0m1+eBKZcikd9PWtXC5DoDotRO04B9YOvFIXmXLy2jEbiqE6Df7DTleA5socLqvEFVxtJyrpZFWz/pHM2CVte0lS8g2eDe6prOyqPglhzROL+Xye4tmT4WvRcQ2/m81p+/rdguOi8Hc5L/8Qk4vhZzy08DduGt9eVQyP2qoTM1zi0/uf4hvBWf5c77e69Gf798y08L7j0RERERERERERH9P99ZpSVRivB/rgAAAABJRU5ErkJggg=="
                  spinner-color="white"
                  style="height: 50px; max-width: 50px" />
         </q-avatar>
       </template>
-      <template v-else-if="col.name === 'actions'">
+      <template v-else-if="inputData.col.name === 'actions'">
         <q-btn round
                flat
                dense
                size="md"
                color="info"
                icon="info"
-               :to="{name: 'User.Show', params: {id: props.row.id}}">
+               :to="{name: 'User.Show', params: {id: inputData.props.row.id}}">
           <q-tooltip>
             مشاهده
           </q-tooltip>
@@ -43,14 +43,14 @@
                color="negative"
                icon="delete"
                class="q-ml-md"
-               @click="showConfirmRemoveDialog(props.row, 'id', getRemoveMessage(props.row))">
+               @click="showConfirmRemoveDialog(inputData.props.row, 'id', getRemoveMessage(inputData.props.row))">
           <q-tooltip>
             حذف
           </q-tooltip>
         </q-btn>
       </template>
       <template v-else>
-        {{ col.value }}
+        {{ inputData.col.value }}
       </template>
     </template>
     <template #entity-index-table-item-cell="{inputData, showConfirmRemoveDialog}">
