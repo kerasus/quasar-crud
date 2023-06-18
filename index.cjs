c4874c36c67c6e2d37631e872f8cf958e0b728ef
JasGH <yasaman.gonehi@gmail.com>
2023-06-18 14:01:53 +0330

    dual entry point added

--- index.cjs
+++ index.cjs
@@ -1,25 +1,15 @@
-import Portlet from './src/components/Portlet.vue'
-import EntityCrud from './src/components/EntityCrud.vue'
-import EntityAction from './src/components/Entity/EntityAction.vue'
-import EntityShow from './src/components/Entity/Show/EntityShow.vue'
-import EntityEdit from './src/components/Entity/Edit/EntityEdit.vue'
-import EntityIndex from './src/components/Entity/Index/EntityIndex.vue'
-import EntityCreate from './src/components/Entity/Create/EntityCreate.vue'
-import EntityInput from './src/components/Entity/Attachment/EntityInput.vue'
-import EntityIndexTable from './src/components/Entity/Index/EntityIndexTable.vue'
-import EntityCrudFormBuilder from './src/components/Entity/EntityCrudFormBuilder.vue'
+const Portlet = require('./src/components/Portlet.vue').default
+const EntityCrud = require('./src/components/EntityCrud.vue').default
+const EntityAction = require('./src/components/Entity/EntityAction.vue').default
+const EntityShow = require('./src/components/Entity/Show/EntityShow.vue').default
+const EntityEdit = require('./src/components/Entity/Edit/EntityEdit.vue').default
+const EntityIndex = require('./src/components/Entity/Index/EntityIndex.vue').default
+const EntityCreate = require('./src/components/Entity/Create/EntityCreate.vue').default
+const EntityInput = require('./src/components/Entity/Attachment/EntityInput.vue').default
+const EntityIndexTable = require('./src/components/Entity/Index/EntityIndexTable.vue').default
+const EntityCrudFormBuilder = require('./src/components/Entity/EntityCrudFormBuilder.vue').default
+
 const EntityCrudObject = { EntityCreate, EntityShow, EntityEdit, EntityIndex, EntityCrud, Portlet, EntityIndexTable, EntityInput, EntityCrudFormBuilder, EntityAction }
 
-export {
-  EntityCrudObject as default,
-  EntityCreate,
-  EntityShow,
-  EntityEdit,
-  EntityIndex,
-  EntityCrud,
-  Portlet,
-  EntityIndexTable,
-  EntityInput,
-  EntityCrudFormBuilder,
-  EntityAction
-}
+module.exports = EntityCrudObject
+module.exports.default = EntityCrudObject
